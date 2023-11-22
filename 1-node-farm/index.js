903fd38ce4aacedfd413e7d5ebf71c0287a91a82
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-11-23 01:12:36 +0300

    some change by preitter

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -39,17 +39,17 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  "utf-8",
 );
 const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  "utf-8",
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  "utf-8",
 );
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
